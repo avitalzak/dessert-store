9a563a0cd2d14c518eb79da88fdb20997c690999
avitalzak <avital.zak.1@gmail.com>
2026-07-19 23:19:20 +0300

    add details to env

---
+++ README.md
@@ -0,0 +1,24 @@
+# Dessert Store 🍰
+
+Full Stack dessert ordering system.
+
+## About The Project
+
+מערכת להזמנת קינוחים המאפשרת למשתמש לבחור בסיס קינוח, להוסיף תוספות וסירופים, לנהל עגלת קניות ולבצע הזמנות.
+
+## Technologies
+
+### Client
+- React
+- Vite
+- Redux Toolkit
+- React Router
+
+### Server
+- Node.js
+- Express
+- MongoDB
+- Mongoose
+- JWT Authentication
+
+## Project Structure
--- server/config.js
+++ server/config.js
@@ -1,6 +1,7 @@
-//export const MONGO_URI = "mongodb+srv://avitalzak1_db_user:56W8ReBYWpVROqgq@cluster0.rdkqydh.mongodb.net/dessertDB";
-export const MONGO_URI =
-"mongodb://avitalzak1_db_user:56W8ReBYWpVROqgq@ac-ab8sudb-shard-00-00.rdkqydh.mongodb.net:27017,ac-ab8sudb-shard-00-01.rdkqydh.mongodb.net:27017,ac-ab8sudb-shard-00-02.rdkqydh.mongodb.net:27017/dessertDB?ssl=true&replicaSet=atlas-qb3t4q-shard-0&authSource=admin&appName=Cluster0";
+require("dotenv").config();
 
-//export const MONGO_URI = "mongodb://127.0.0.1:27017/dessertDB";
+const mongoose = require("mongoose");
 
+mongoose.connect(process.env.MONGO_URI)
+.then(() => console.log("MongoDB connected"))
+.catch(err => console.log(err));
